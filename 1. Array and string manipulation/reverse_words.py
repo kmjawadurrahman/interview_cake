3441e5abac1da1f1ca5dfd8c3317f127cd7f8555
K M Jawadur Rahman <jawadur@gmail.com>
2020-09-02 21:55:22 +0600

    feat: Reverse_words; reflections and notes pending

---
+++ 1. Array and string manipulation/reverse_words.py
@@ -0,0 +1,81 @@
+import unittest
+
+
+def reverse_words(message):
+
+    # Decode the message by reversing the words
+    message_len = len(message)
+
+    if message_len==0:
+        return
+
+    insert_index = 0
+    message.insert(insert_index, ' ')
+    for idx in range(message_len):
+        if message[-1]==" ":
+            insert_index = idx+1
+
+        popped_letter = message.pop()
+        message.insert(insert_index, popped_letter)
+
+    message.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_one_word(self):
+        message = list('vault')
+        reverse_words(message)
+        expected = list('vault')
+        self.assertEqual(message, expected)
+
+    def test_two_words(self):
+        message = list('thief cake')
+        reverse_words(message)
+        expected = list('cake thief')
+        self.assertEqual(message, expected)
+
+    def test_three_words(self):
+        message = list('one another get')
+        reverse_words(message)
+        expected = list('get another one')
+        self.assertEqual(message, expected)
+
+    def test_multiple_words_same_length(self):
+        message = list('rat the ate cat the')
+        reverse_words(message)
+        expected = list('the cat ate the rat')
+        self.assertEqual(message, expected)
+
+    def test_multiple_words_different_lengths(self):
+        message = list('yummy is cake bundt chocolate')
+        reverse_words(message)
+        expected = list('chocolate bundt cake is yummy')
+        self.assertEqual(message, expected)
+
+    def test_empty_string(self):
+        message = list('')
+        reverse_words(message)
+        expected = list('')
+        self.assertEqual(message, expected)
+
+
+unittest.main(verbosity=2)
