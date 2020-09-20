3ed77709243d6f485ef845e7fd65dc30ca83cbc8
K M Jawadur Rahman <jawadur@gmail.com>
2020-09-20 20:19:03 +0600

    feat: Inflight_entertainment; reflections and notes pending

---
+++ 2. Hashing and hash tables/README.md
@@ -0,0 +1,3 @@
+# Inflight Entertainment
+
+### Notes:
---
+++ 2. Hashing and hash tables/inflight_entertainment.py
@@ -0,0 +1,74 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+
+    # Determine if two movie runtimes add up to the flight length
+    d = {}
+    for idx, movie_len in enumerate(movie_lengths):
+        remaining_len = flight_length - movie_len
+        d[remaining_len] = idx
+
+    for idx, movie_len in enumerate(movie_lengths):
+        if movie_len in d:
+            # to ensure that same movie is not shown twice
+            if d[movie_len] != idx:
+                return True
+
+    return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
